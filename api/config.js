766f2c1dc051def8a82bdf9481a00240db4f2c71
grandot <jw.grandot@gmail.com>
2025-04-27 15:10:37 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,7 +1,9 @@
-{
-  "version": 2,
-  "builds": [
-    { "src": "api/**/*.js", "use": "@vercel/node" },
-    { "src": "api/**/*.ts", "use": "@vercel/node" }
-  ]
+// api/config.js
+export default function handler(req, res) {
+  res
+    .status(200)
+    .json({
+      PUBLIC_GMAPS_API_KEY: process.env.GMAPS_API_KEY,
+      SUPABASE_ANON_KEY:   process.env.SUPABASE_ANON_KEY
+    });
 }
